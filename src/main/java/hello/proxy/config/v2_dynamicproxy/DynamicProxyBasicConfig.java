d15888eebf9d868698b0089afe26c4979aad216d
Minxhvk <minxhvk@gmail.com>
2023-11-06 23:27:55 +0900

    JDK Dynamic Proxy

--- src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyBasicConfig.java
+++ src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyBasicConfig.java
@@ -13,7 +13,7 @@ public class DynamicProxyBasicConfig {
 
     @Bean
     public OrderControllerV1 orderControllerV1(LogTrace logTrace) {
-        OrderControllerV1 orderController = new  (orderServiceV1(logTrace));
+        OrderControllerV1 orderController = new OrderControllerV1Impl(orderServiceV1(logTrace));
         OrderControllerV1 proxy = (OrderControllerV1) Proxy.newProxyInstance(OrderControllerV1.class.getClassLoader(),
                 new Class[]{OrderControllerV1.class},
                 new LogTraceBasicHandler(orderController, logTrace));
